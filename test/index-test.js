fe652d731780ac611598bfbcc950aaef1018a82f
Steven DeMartini <steven.demartini@airbnb.com>
2017-06-20 17:35:01 -0700

    Add tests for renderReactWithAphroditeStatic

--- test/index-test.js
+++ test/index-test.js
@@ -2,7 +2,7 @@ import jsdom from 'jsdom';
 import { assert } from 'chai';
 
 import AphroditeComponent from './components/AphroditeComponent';
-import { renderReactWithAphrodite } from '../';
+import { renderReactWithAphrodite, renderReactWithAphroditeStatic } from '../';
 
 describe('aphrodite css rendering', () => {
   let result;
@@ -17,6 +17,7 @@ describe('aphrodite css rendering', () => {
     assert.isString(result);
 
     assert.ok(/style data-aphrodite/.test(result));
+    assert.ok(/Zack/.test(result));
     assert.ok(/data-aphrodite-css/.test(result));
   });
 
@@ -31,6 +32,43 @@ describe('aphrodite css rendering', () => {
       global.document = window.document;
 
       renderReactWithAphrodite('AC', AphroditeComponent);
+
+      done();
+    });
+  });
+});
+
+describe('renderReactWithAphroditeStatic', () => {
+  let result;
+  beforeEach(() => {
+    result = renderReactWithAphroditeStatic('AC', AphroditeComponent)({
+      children: ['Steven'],
+      onPress() { console.log('Clicked'); },
+    });
+  });
+
+  it('the markup looks good', () => {
+    assert.isString(result);
+
+    assert.ok(/style data-aphrodite/.test(result));
+    assert.ok(/Steven/.test(result));
+  });
+
+  it('the markup does not contain aphrodite-css info for the client', () => {
+    assert.isFalse(/data-aphrodite-css/.test(result));
+  });
+
+  it('returns nothing when used on the client', (done) => {
+    jsdom.env(result, (err, window) => {
+      if (err) {
+        done(err);
+        return;
+      }
+
+      global.window = window;
+      global.document = window.document;
+
+      assert.isUndefined(renderReactWithAphroditeStatic('AC', AphroditeComponent));
 
       done();
     });
