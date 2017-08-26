4d68f0fb4a67cfd11cd5e1d201c4bc470bebb2cb
Jordan Harband <ljharb@gmail.com>
2017-08-26 14:31:48 -0700

    [Tests] use `prop-types` instead of `React.PropTypes`

--- test/components/AphroditeComponent.jsx
+++ test/components/AphroditeComponent.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { css, StyleSheet } from 'aphrodite';
 
 const styles = StyleSheet.create({
