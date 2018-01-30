820c73cb140a549bc185cc0a6f67535b5d70b69d
Nora Tarano <nora.tarano@airbnb.com>
2018-01-29 23:45:03 -0800

    Add rendering configuration for pre-render enhancers
    - Allows to wrap components with enhancer HOCs before rendering both
      client- and server-side
    - Can be configured from consumer applications using the provided
      configuration functon
    - Adds tests

--- src/index.js
+++ src/index.js
@@ -4,52 +4,70 @@ import ReactDOMServer from 'react-dom/server';
 import hypernova, { serialize, load, toScript, fromScript } from 'hypernova';
 import { StyleSheet, StyleSheetServer } from 'aphrodite';
 
-export const renderReactWithAphrodite = (name, component) => hypernova({
-  server() {
-    return (props) => {
-      const { html, css } = StyleSheetServer.renderStatic(() => (
-        ReactDOMServer.renderToString(React.createElement(component, props))
-      ));
+const config = {
+  enhancers: [], // HOCs applied to the component before render: e0(e1(e2(...en(component))))
+};
 
-      const style = `<style data-aphrodite="data-aphrodite">${css.content}</style>`;
-      const markup = serialize(name, html, props);
-      const classNames = toScript({ 'aphrodite-css': name }, css.renderedClassNames);
+export const setRenderEnhancers = (...enhancers) => {
+  config.enhancers = enhancers.filter(hoc => typeof hoc === 'function'); // clear & set
+};
 
-      return `${style}\n${markup}\n${classNames}`;
-    };
-  },
+const enhance = component => config.enhancers.reverse().reduce((x, f) => f(x), component);
 
-  client() {
-    const classNames = fromScript({ 'aphrodite-css': name });
-    if (classNames) StyleSheet.rehydrate(classNames);
+export const renderReactWithAphrodite = (name, component) => {
+  const enhancedComponent = enhance(component);
+  return hypernova({
+    server() {
+      return (props) => {
+        const { html, css } = StyleSheetServer.renderStatic(() => {
+          const element = React.createElement(enhancedComponent, props);
+          return ReactDOMServer.renderToString(element);
+        });
 
-    const payloads = load(name);
-    if (payloads) {
-      payloads.forEach((payload) => {
-        const { node, data } = payload;
-        if (node) {
-          const element = React.createElement(component, data);
-          ReactDOM.render(element, node);
-        }
-      });
-    }
+        const style = `<style data-aphrodite="data-aphrodite">${css.content}</style>`;
+        const markup = serialize(name, html, props);
+        const classNames = toScript({ 'aphrodite-css': name }, css.renderedClassNames);
 
-    return component;
-  },
-});
+        return `${style}\n${markup}\n${classNames}`;
+      };
+    },
 
-export const renderReactWithAphroditeStatic = (name, component) => hypernova({
-  server() {
-    return (props) => {
-      const { html, css } = StyleSheetServer.renderStatic(() => (
-        ReactDOMServer.renderToStaticMarkup(React.createElement(component, props))
-      ));
+    client() {
+      const classNames = fromScript({ 'aphrodite-css': name });
+      if (classNames) StyleSheet.rehydrate(classNames);
 
-      const style = `<style data-aphrodite="data-aphrodite">${css.content}</style>`;
+      const payloads = load(name);
+      if (payloads) {
+        payloads.forEach((payload) => {
+          const { node, data } = payload;
+          if (node) {
+            const element = React.createElement(enhancedComponent, data);
+            ReactDOM.render(element, node);
+          }
+        });
+      }
 
-      return `${style}\n${html}`;
-    };
-  },
+      return component;
+    },
+  });
+};
 
-  client() {},
-});
+export const renderReactWithAphroditeStatic = (name, component) => {
+  const enhancedComponent = enhance(component);
+  return hypernova({
+    server() {
+      return (props) => {
+        const { html, css } = StyleSheetServer.renderStatic(() => {
+          const element = React.createElement(enhancedComponent, props);
+          return ReactDOMServer.renderToStaticMarkup(element);
+        });
+
+        const style = `<style data-aphrodite="data-aphrodite">${css.content}</style>`;
+
+        return `${style}\n${html}`;
+      };
+    },
+
+    client() {},
+  });
+};
